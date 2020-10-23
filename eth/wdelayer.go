82ee7c6aa765ff89d4d2693dbcf92a95c21d8f65
laisolizq <laisolizq@gmail.com>
2020-10-23 17:46:34 +0200

    Add struct get state endpoint

--- eth/wdelayer.go
+++ eth/wdelayer.go
@@ -16,7 +16,7 @@ import (
 	WithdrawalDelayer "github.com/hermeznetwork/hermez-node/eth/contracts/withdrawdelayer"
 )
 
-// WDelayerConstants are the constants of the Rollup Smart Contract
+// WDelayerConstants are the constants of the Withdrawal Delayer Smart Contract
 type WDelayerConstants struct {
 	// Max Withdrawal Delay
 	MaxWithdrawalDelay uint64 `json:"maxWithdrawalDelay"`
@@ -24,6 +24,17 @@ type WDelayerConstants struct {
 	MaxEmergencyModeTime uint64 `json:"maxEmergencyModeTime"`
 	// HermezRollup smartcontract address
 	HermezRollup ethCommon.Address `json:"hermezRollup"`
+}
+
+// WDelayerVariables are the variables of the Withdrawal Delayer Smart Contract
+type WDelayerVariables struct {
+	HermezRollupAddress        ethCommon.Address `json:"hermezRollupAddress" meddler:"rollup_address"`
+	HermezGovernanceDAOAddress ethCommon.Address `json:"hermezGovernanceDAOAddress" meddler:"govdao_address"`
+	WhiteHackGroupAddress      ethCommon.Address `json:"whiteHackGroupAddress" meddler:"whg_address"`
+	HermezKeeperAddress        ethCommon.Address `json:"hermezKeeperAddress" meddler:"keeper_address"`
+	WithdrawalDelay            uint64            `json:"withdrawalDelay" meddler:"withdrawal_delay"`
+	EmergencyModeStartingTime  uint64            `json:"emergencyModeStartingTime" meddler:"emergency_start_time"`
+	EmergencyMode              bool              `json:"emergencyMode" meddler:"emergency_mode"`
 }
 
 // DepositState is the state of Deposit
