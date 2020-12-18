1a003117530df4919313e0d6f4983d92483dcf63
ToniRamirezM <toni@iden3.com>
2020-12-18 20:00:28 +0100

    Change metrics calculation when less than 24 hours running

--- db/historydb/historydb.go
+++ db/historydb/historydb.go
@@ -1768,16 +1768,24 @@ func (hdb *HistoryDB) GetAccountsAPI(
 func (hdb *HistoryDB) GetMetrics(lastBatchNum common.BatchNum) (*Metrics, error) {
 	metricsTotals := &MetricsTotals{}
 	metrics := &Metrics{}
+
 	err := meddler.QueryRow(
 		hdb.db, metricsTotals, `SELECT COUNT(tx.*) as total_txs,
-		COALESCE (MIN(tx.batch_num), 0) as batch_num 
+		COALESCE (MIN(tx.batch_num), 0) as batch_num, COALESCE (MIN(block.timestamp), NOW()) AS min_timestamp, COALESCE (MAX(block.timestamp), NOW()) AS max_timestamp 
 		FROM tx INNER JOIN block ON tx.eth_block_num = block.eth_block_num
 		WHERE block.timestamp >= NOW() - INTERVAL '24 HOURS';`)
 	if err != nil {
 		return nil, tracerr.Wrap(err)
 	}
 
-	metrics.TransactionsPerSecond = float64(metricsTotals.TotalTransactions / (24 * 60 * 60))
+	seconds := metricsTotals.MaxTimestamp.Sub(metricsTotals.MinTimestamp).Seconds()
+
+	// Avoid dividing by 0
+	if seconds == 0 {
+		seconds++
+	}
+
+	metrics.TransactionsPerSecond = float64(float64(metricsTotals.TotalTransactions) / seconds)
 	if (lastBatchNum - metricsTotals.FirstBatchNum) > 0 {
 		metrics.TransactionsPerBatch = float64(int64(metricsTotals.TotalTransactions) /
 			int64(lastBatchNum-metricsTotals.FirstBatchNum))
@@ -1793,7 +1801,7 @@ func (hdb *HistoryDB) GetMetrics(lastBatchNum common.BatchNum) (*Metrics, error)
 		return nil, tracerr.Wrap(err)
 	}
 	if metricsTotals.TotalBatches > 0 {
-		metrics.BatchFrequency = float64((24 * 60 * 60) / metricsTotals.TotalBatches)
+		metrics.BatchFrequency = seconds / float64(metricsTotals.TotalBatches)
 	} else {
 		metrics.BatchFrequency = 0
 	}
