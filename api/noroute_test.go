5f9336160d40a1d7e29408624cd418959583a181
Mikelle <mikhwall@gmail.com>
2021-03-25 22:41:46 +0300

    added test and move logic to api.NewAPI

---
+++ api/noroute_test.go
@@ -0,0 +1,29 @@
+package api
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNoRouteVersionNotProvided(t *testing.T) {
+	endpoint := apiIP + apiPort + "/"
+	// not using doGoodReq, bcs internally
+	// there is a method FindRoute that checks route and returns error
+	resp, err := doSimpleReq("GET", endpoint)
+	assert.NoError(t, err)
+	assert.Equal(t,
+		"{\"error\":\"Version not provided, please provide a valid version in the path such as v1\"}\n",
+		resp)
+}
+
+func TestNoRoute(t *testing.T) {
+	endpoint := apiURL
+	// not using doGoodReq, bcs internally
+	// there is a method FindRoute that checks route and returns error
+	resp, err := doSimpleReq("GET", endpoint)
+	assert.NoError(t, err)
+	assert.Equal(t,
+		"{\"error\":\"404 page not found\"}\n",
+		resp)
+}
