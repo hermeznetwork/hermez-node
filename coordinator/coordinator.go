949bb653f0fc73495349301d6cf07974d315f4f4
Eduard S <eduard@iden3.io>
2021-04-12 16:51:07 +0200

    Reset pool txs with batchNum forged externally

--- coordinator/coordinator.go
+++ coordinator/coordinator.go
@@ -490,6 +490,34 @@ func (c *Coordinator) handleMsgSyncBlock(ctx context.Context, msg *MsgSyncBlock)
 	if c.pipeline != nil {
 		c.pipeline.SetSyncStatsVars(ctx, &msg.Stats, &msg.Vars)
 	}
+
+	// If there's any batch not forged by us, make sure we don't keep
+	// "phantom forged l2txs" in the pool.  That is, l2txs that we
+	// attempted to forge in BatchNum=N, where the forgeBatch transaction
+	// failed, but another batch with BatchNum=N was forged by another
+	// coordinator successfully.
+	externalBatchNums := []common.BatchNum{}
+	for _, batch := range msg.Batches {
+		if batch.Batch.ForgerAddr != c.cfg.ForgerAddress {
+			externalBatchNums = append(externalBatchNums, batch.Batch.BatchNum)
+		}
+	}
+	if len(externalBatchNums) > 0 {
+		// If we just synced external batches, make sure the pipeline
+		// is stopped
+		lastValidBatch := externalBatchNums[0] - 1
+		if c.pipeline != nil {
+			if err := c.handleStopPipeline(ctx, "synced external batches",
+				lastValidBatch); err != nil {
+				return err
+			}
+		} else {
+			if err := c.l2DB.Reorg(lastValidBatch); err != nil {
+				return err
+			}
+		}
+	}
+
 	if !c.stats.Synced() {
 		return nil
 	}
