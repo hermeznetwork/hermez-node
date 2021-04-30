cf1212106c0ff99879c7c7853385e302b9df72fa
Mikhail Wall <mikhwall@gmail.com>
2021-04-30 14:37:44 +0300

    implemented health endpoint (#764)

---
+++ api/health_test.go
@@ -0,0 +1,38 @@
+package api
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+type healthResponse struct {
+	HistoryDB struct {
+		LastMigration string `json:"last_migration"`
+		Status        string `json:"status"`
+		Version       string `json:"version"`
+	} `json:"historyDB"`
+	L2DB struct {
+		LastMigration string `json:"last_migration"`
+		Status        string `json:"status"`
+		Version       string `json:"version"`
+	} `json:"l2DB"`
+	Status    string    `json:"status"`
+	Timestamp time.Time `json:"timestamp"`
+	Version   string    `json:"version"`
+}
+
+func TestHealth(t *testing.T) {
+	endpoint := apiURL + "health"
+	var healthResponseTest healthResponse
+	err := doGoodReq("GET", endpoint, nil, &healthResponseTest)
+
+	assert.NoError(t, err)
+	assert.NotNil(t, healthResponseTest.L2DB.LastMigration)
+	assert.Equal(t, healthResponseTest.L2DB.Status, "UP")
+	assert.NotNil(t, healthResponseTest.HistoryDB.LastMigration)
+	assert.Equal(t, healthResponseTest.HistoryDB.Status, "UP")
+	assert.Equal(t, healthResponseTest.Version, "test")
+	assert.Equal(t, healthResponseTest.Status, "UP")
+}
