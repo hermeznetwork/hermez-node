19eb01c85d2bcde209d7d0fb7cfa764c7f31548e
arnaubennassar <arnaubennassar5@gmail.com>
2021-05-06 18:30:48 +0200

    Add CLI command to downgrade SQL schema

--- test/dbUtils.go
+++ test/dbUtils.go
@@ -27,7 +27,7 @@ func AssertUSD(t *testing.T, expected, actual *float64) {
 // WipeDB redo all the migrations of the SQL DB (HistoryDB and L2DB),
 // efectively recreating the original state
 func WipeDB(db *sqlx.DB) {
-	if err := dbUtils.MigrationsDown(db.DB); err != nil {
+	if err := dbUtils.MigrationsDown(db.DB, 0); err != nil {
 		panic(err)
 	}
 	if err := dbUtils.MigrationsUp(db.DB); err != nil {
