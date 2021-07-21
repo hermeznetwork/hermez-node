0d1e8dbd5093aa6d4666267ac3d0bca18d66ca11
a_bennassar <arnaubennassar5@gmail.com>
2021-07-21 16:24:35 +0200

    Check atomic groups integrity (#1015)
    * Check atomic groups integrity

--- txselector/errors.go
+++ txselector/errors.go
@@ -65,4 +65,11 @@ const (
 	ErrNoAvailableSlotsCode int = 17
 	// ErrNoAvailableSlotsType error type
 	ErrNoAvailableSlotsType string = "ErrNoAvailableSlots"
+
+	// ErrInvalidAtomicGroup error message returned if an atomic group is malformed
+	ErrInvalidAtomicGroup = "Tx not selected because it belongs to an atomic group with missing transactions or bad requested transaction"
+	// ErrInvalidAtomicGroupCode error code
+	ErrInvalidAtomicGroupCode int = 18
+	// ErrInvalidAtomicGroupType error type
+	ErrInvalidAtomicGroupType string = "ErrInvalidAtomicGroup"
 )
