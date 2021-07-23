53b6f80a91a6be0f1e8d8c84bbaed44c525b9677
Alonso Rodriguez <ARR552@users.noreply.github.com>
2021-07-23 12:10:25 +0200

    abi's included and update script fixed (#1027)

--- eth/contracts/update.sh
+++ eth/contracts/update.sh
@@ -3,26 +3,18 @@
 set -e
 
 gen() {
-    local name=$1
-    local pkg=$2
-    local package=$3
-    local folder=$4
-    if [ -z "$4" ]; then
+    local pkg=$1
+    local package=$2
+    local folder=$3
+    if [ -z "$3" ]; then
         folder=$package
     fi
 
-    jq .abi "${CONTRACTS}/artifacts/contracts/${name}/${pkg}.sol/${pkg}.json" > /tmp/${name}.abi
-    abigen --abi /tmp/${name}.abi --pkg=${package} --out=${folder}/${package}.go
+    jq .abi "abi/${pkg}.json" > /tmp/${package}.abi
+    abigen --abi /tmp/${package}.abi --pkg=${package} --out=${folder}/${package}.go
 }
 
-if [ "$1" = "" ]; then
-    echo "Usage: $0 CONTRACTS_REPO_PATH"
-    exit 1
-fi
-
-CONTRACTS="$1"
-
-gen hermez Hermez hermez
-gen auction HermezAuctionProtocol auction
-gen withdrawalDelayer WithdrawalDelayer withdrawaldelayer
-gen HEZ HEZ tokenhez
+gen Hermez hermez
+gen HermezAuctionProtocol auction
+gen WithdrawalDelayer withdrawaldelayer
+gen HEZ tokenhez
