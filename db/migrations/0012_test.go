92c41a09c6b9b3d6b0e8d74ca659a5192108bb04
Mário Idival <marioidival@gmail.com>
2021-09-20 07:27:50 -0300

    fix: tests with migration

--- db/migrations/0012_test.go
+++ db/migrations/0012_test.go
@@ -7,17 +7,10 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-// This migration adds the column `gas_price` and `gas_used` on `batch` table
-
 type migrationTest0012 struct{}
 
 func (m migrationTest0012) InsertData(db *sqlx.DB) error {
-	// insert tx
-	const queryInsert = `
-    INSERT INTO provers (public_dns, instance_id) VALUES ('http://some-url-on-aws:9080', 'id-some-instance-id');
-	`
-	_, err := db.Exec(queryInsert)
-	return err
+	return nil
 }
 
 func (m migrationTest0012) RunAssertsAfterMigrationUp(t *testing.T, db *sqlx.DB) {
@@ -25,21 +18,25 @@ func (m migrationTest0012) RunAssertsAfterMigrationUp(t *testing.T, db *sqlx.DB)
 	row := db.QueryRow(queryGetProvers)
 	var result int
 	assert.NoError(t, row.Scan(&result))
-	assert.Equal(t, 1, result)
+	assert.Equal(t, 0, result)
 
 	insert := `
     INSERT INTO provers (public_dns, instance_id) VALUES ('http://some-url-on-aws:9080', 'id-some-instance-id2');
     `
 	_, err := db.Exec(insert)
 	assert.NoError(t, err)
+
+	row = db.QueryRow(queryGetProvers)
+	assert.NoError(t, row.Scan(&result))
+	assert.Equal(t, 1, result)
 }
 
 func (m migrationTest0012) RunAssertsAfterMigrationDown(t *testing.T, db *sqlx.DB) {
-	// check that the batch inserted in previous step is persisted with same content
 	var result int
 	const queryGetProvers = `SELECT COUNT(*) FROM provers;`
 	row := db.QueryRow(queryGetProvers)
-	assert.Equal(t, `pq: relation "provers" does not exist`, row.Scan(&result).Error())
+	err := row.Scan(&result)
+	assert.Equal(t, `pq: relation "provers" does not exist`, err.Error())
 }
 
 func TestMigration0012(t *testing.T) {
