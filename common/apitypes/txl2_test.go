25df2e1c9f253f5b0b8ed10a724cd3a153099031
Alonso <ARR551@protonmail.com>
2021-09-22 11:26:13 +0200

    Test

--- common/apitypes/txl2_test.go
+++ common/apitypes/txl2_test.go
@@ -3,19 +3,33 @@ package apitypes
 import (
 	"testing"
 
+	"github.com/hermeznetwork/hermez-node/common"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestMarshall(t *testing.T) {
-	var l2tx TxL2
-	_, err := l2tx.MarshalJSON()
+func TestMarshalUnmarshal(t *testing.T) {
+	txID, err := common.NewTxIDFromString("0x02c4ebd603b30d759a5a81622f7e4dee46bb1cb5cb756f45dcee9134893cd6a093")
 	assert.NoError(t, err)
-}
-
-func TestUnmarshall(t *testing.T) {
-	var l2tx, dest TxL2
+	var token = token{
+		TokenName:        "Token name",
+		TokenSymbol:      "TKSY",
+		TokenEthBlockNum: 23,
+	}
+	var l2tx = TxL2{
+		ItemID: 65,
+		TxID:   txID,
+		Token:  token,
+	}
 	bytes, err := l2tx.MarshalJSON()
 	assert.NoError(t, err)
+
+	var dest TxL2
 	err = dest.UnmarshalJSON(bytes)
 	assert.NoError(t, err)
+	assert.Equal(t, l2tx.Token.TokenName, dest.Token.TokenName)
+	assert.Equal(t, l2tx.Token.TokenSymbol, dest.Token.TokenSymbol)
+	assert.Equal(t, l2tx.Token.TokenEthBlockNum, dest.Token.TokenEthBlockNum)
+	assert.Equal(t, l2tx.ItemID, dest.ItemID)
+	assert.Equal(t, l2tx.TxID, dest.TxID)
+	assert.Equal(t, l2tx, dest)
 }
