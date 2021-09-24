d645536409681100b0da448a8c1882787d9c17ac
Mário Idival <marioidival@gmail.com>
2021-09-24 06:47:11 -0300

    refactor: looking for more status from prover server

--- db/historydb/provers.go
+++ db/historydb/provers.go
@@ -13,7 +13,7 @@ type provers struct {
 func (hdb *HistoryDB) GetProvers() ([]string, error) {
 	var provers []*provers
 	err := meddler.QueryAll(
-		hdb.dbRead, &provers, "SELECT public_dns FROM provers WHERE status = 'ready';",
+		hdb.dbRead, &provers, "SELECT public_dns FROM provers WHERE status IN ('ready', 'success', 'failed', 'aborted', 'unverified');",
 	)
 	var publicDNS []string
 	for _, prover := range provers {
